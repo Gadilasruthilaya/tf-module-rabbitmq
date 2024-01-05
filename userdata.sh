0ba2f298d6ee6029c7b5cbc8e50e7249efdd92ce
sruthilaya <sruthilayagadila@gmail.com>
2024-01-05 14:15:45 +0530

    rabbitmq

--- userdata.sh
+++ userdata.sh
@@ -2,5 +2,4 @@
 
 set-hostname -skip-apply ${component}
 labauto ansible
-ansible-pull -i localhost, -U http://github.com/Gadilasruthilaya/roboshopshell-ansible-v1.git  main.yml -e role_name=rabbitmq -e
- env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U http://github.com/Gadilasruthilaya/roboshopshell-ansible-v1.git  main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
